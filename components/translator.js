e4011569321f212afd36ac1e9de7cc0e1b29b441
Melody Davis <piano.fan122@gmail.com>
2022-06-22 16:28:00 -0400

    time can now translate

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,42 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require('./american-to-british-titles.js');
+const britishOnly = require('./british-only.js');
 
 class Translator {
+	timeRegex(textString, locale) {
+		let newSymbol;
 
+		// check the locale to update time symbol
+		if (locale === 'american-to-british') {
+			newSymbol = '.';
+		}
+		if (locale === 'british-to-american') {
+			newSymbol = ':';
+		}
+
+		// replace old time symbol
+		const timeRegex = /(\d{1,2})(:?.?)(\d{2})/g;
+		const newTime = textString.replace(timeRegex, `$1${newSymbol}$3`);
+
+		// wrap new time in span
+		const spanRegex = newTime.match(/(\d{1,2}:?.?\d{2})/g);
+		const newString = newTime.replace(
+			spanRegex,
+			`<span class="highlight">${spanRegex}</span>`
+		);
+		return newString;
+	}
+
+	translate(textString, locale) {
+		let newMessage = textString;
+
+		// check if string contains time
+		if (textString.match(/\d{1,2}:?.?\d{2}/)) {
+			newMessage = this.timeRegex(newMessage, locale);
+		}
+		return newMessage;
+	}
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,12 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+	const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+	app.route('/api/translate').post((req, res) => {
+		const text = req.body.text;
+		const locale = req.body.locale;
+		const translated = translator.translate(text, locale);
+		res.json({ text: text, translation: translated });
+	});
 };
