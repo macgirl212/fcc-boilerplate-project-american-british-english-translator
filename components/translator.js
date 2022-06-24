f56b42ee6376565c62b277d11518a27d9e8f1f34
Melody Davis <piano.fan122@gmail.com>
2022-06-24 13:21:59 -0400

    translator can now replace words

--- components/translator.js
+++ components/translator.js
@@ -4,6 +4,133 @@ const americanToBritishTitles = require('./american-to-british-titles.js');
 const britishOnly = require('./british-only.js');
 
 class Translator {
+	// return word array with replaced word
+	replaceWord(arr, index, newWord) {
+		return [
+			...arr.slice(0, index),
+			`<span class="highlight">${newWord}</span>`,
+			...arr.slice(index + 1),
+		];
+	}
+
+	replaceHonorifics(textString, locale) {
+		let textArray = textString.split(' ');
+
+		if (locale === 'american-to-british') {
+			// find all keys in object
+			for (const [key, value] of Object.entries(americanToBritishTitles)) {
+				// check each word and compare with key
+				for (let i = 0; i < textArray.length; i++) {
+					if (key === textArray[i].toLowerCase()) {
+						// create new honorific and replace
+						const newHonorific = textArray[i].slice(0, -1);
+						textArray = this.replaceWord(textArray, i, newHonorific);
+					}
+				}
+			}
+		}
+
+		if (locale === 'british-to-american') {
+			// find all values in object
+			for (const [key, value] of Object.entries(americanToBritishTitles)) {
+				//check each word and compare with value
+				for (let i = 0; i < textArray.length; i++) {
+					if (value === textArray[i].toLowerCase()) {
+						// create new honorific and replace
+						const newHonorific = `${textArray[i]}.`;
+						textArray = this.replaceWord(textArray, i, newHonorific);
+					}
+				}
+			}
+		}
+
+		return textArray.join(' ');
+	}
+
+	replaceAmericanTerms(textString, locale) {
+		if (locale === 'american-to-british') {
+			// find all keys in object
+			for (const [key, value] of Object.entries(americanOnly)) {
+				// if a key matches any terms, replace it
+				if (textString.search(key) !== -1) {
+					textString = textString.replace(
+						key,
+						`<span class="highlight">${value}</span>`
+					);
+				}
+			}
+		}
+		if (locale === 'british-to-american') {
+			for (const [key, value] of Object.entries(americanOnly)) {
+				// find all values in object
+				if (textString.search(value) !== -1) {
+					// if a value matches any terms, replace it
+					textString = textString.replace(
+						value,
+						`<span class="highlight">${key}</span>`
+					);
+				}
+			}
+		}
+		return textString;
+	}
+
+	replaceBritishTerms(textString, locale) {
+		if (locale === 'british-to-american') {
+			// find all keys in object
+			for (const [key, value] of Object.entries(britishOnly)) {
+				// if a key matches any terms, replace it
+				if (textString.search(key) !== -1) {
+					textString = textString.replace(
+						key,
+						`<span class="highlight">${value}</span>`
+					);
+				}
+			}
+		}
+		if (locale === 'american-to-british') {
+			for (const [key, value] of Object.entries(britishOnly)) {
+				// find all values in object
+				if (textString.search(value) !== -1) {
+					// if a value matches any terms, replace it
+					textString = textString.replace(
+						value,
+						`<span class="highlight">${key}</span>`
+					);
+				}
+			}
+		}
+		return textString;
+	}
+
+	replaceSpelling(textString, locale) {
+		if (locale === 'american-to-british') {
+			// find all keys in object
+			for (const [key, value] of Object.entries(americanToBritishSpelling)) {
+				// if a key matches any terms, replace it
+				if (textString.search(key) !== -1) {
+					textString = textString.replace(
+						key,
+						`<span class="highlight">${value}</span>`
+					);
+				}
+			}
+		}
+		if (locale === 'british-to-american') {
+			for (const [key, value] of Object.entries(americanToBritishSpelling)) {
+				// find all values in object
+				if (textString.search(value) !== -1) {
+					// if a value matches any terms, replace it
+					textString = textString.replace(
+						value,
+						`<span class="highlight">${key}</span>`
+					);
+				}
+			}
+		}
+		return textString;
+	}
+
 	timeRegex(textString, locale) {
 		let newSymbol;
 
@@ -31,8 +158,14 @@ class Translator {
 	translate(textString, locale) {
 		let newMessage = textString;
 
+		// replace words based on categories
+		newMessage = this.replaceHonorifics(newMessage, locale);
+		newMessage = this.replaceAmericanTerms(newMessage, locale);
+		newMessage = this.replaceBritishTerms(newMessage, locale);
+		newMessage = this.replaceSpelling(newMessage, locale);
+
 		// check if string contains time
-		if (textString.match(/\d{1,2}:?.?\d{2}/)) {
+		if (newMessage.match(/\d{1,2}:?.?\d{2}/)) {
 			newMessage = this.timeRegex(newMessage, locale);
 		}
 		return newMessage;
