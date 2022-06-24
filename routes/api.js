39370683240d2f54a84637a81a79fda7d44830ef
Melody Davis <piano.fan122@gmail.com>
2022-06-23 23:06:24 -0400

    added error messages

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,16 @@ module.exports = function (app) {
 	app.route('/api/translate').post((req, res) => {
 		const text = req.body.text;
 		const locale = req.body.locale;
+
+		// error messages
+		if (!text) {
+			return res.json({ error: 'No text to translate' });
+		}
+
+		if (locale !== 'american-to-british' || locale !== 'british-to-american') {
+			return res.json({ error: 'Invalid value for locale field' });
+		}
+
 		const translated = translator.translate(text, locale);
 		res.json({ text: text, translation: translated });
 	});
