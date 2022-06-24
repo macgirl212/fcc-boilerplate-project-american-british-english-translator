113ee50d92395d8e6e59b3a528fa078209c0ec8a
Melody Davis <piano.fan122@gmail.com>
2022-06-23 23:08:58 -0400

    fixed locale error message

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
 			return res.json({ error: 'No text to translate' });
 		}
 
-		if (locale !== 'american-to-british' || locale !== 'british-to-american') {
+		if (locale !== 'american-to-british' && locale !== 'british-to-american') {
 			return res.json({ error: 'Invalid value for locale field' });
 		}
 
